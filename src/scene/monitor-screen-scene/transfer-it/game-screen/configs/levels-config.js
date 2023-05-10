331c8801ebebedc842edbf5148c7eb2f010f7913
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2023-05-11 01:51:58 +0300

    Added 5 more levels for transfer it

--- src/scene/monitor-screen-scene/transfer-it/game-screen/configs/levels-config.js
+++ src/scene/monitor-screen-scene/transfer-it/game-screen/configs/levels-config.js
@@ -58,6 +58,7 @@ const LEVELS_CONFIG = [
       angle: 0,
     },
   },
+
   // level 2
   {
     roomType: ROOM_TYPE.SMALL,
@@ -104,6 +105,7 @@ const LEVELS_CONFIG = [
       angle: 90,
     },
   },
+
   // level 3
   {
     roomType: ROOM_TYPE.SMALL,
@@ -150,6 +152,7 @@ const LEVELS_CONFIG = [
       angle: 0,
     },
   },
+
   // level 4
   {
     roomType: ROOM_TYPE.SMALL,
@@ -196,6 +199,7 @@ const LEVELS_CONFIG = [
       angle: 180,
     },
   },
+
   // level 5
   {
     roomType: ROOM_TYPE.SMALL,
@@ -250,6 +254,324 @@ const LEVELS_CONFIG = [
       enabled: true,
       position: { x: 0, z: 0 },
       angle: 180,
+    },
+  },
+
+  // level 6
+  {
+    roomType: ROOM_TYPE.SMALL,
+    furniture: [
+      {
+        type: FURNITURE_TYPE.chair,
+        targetGridPosition: { x: 1, z: 4 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.table_big,
+        targetGridPosition: { x: 3, z: 4 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+      {
+        type: FURNITURE_TYPE.table2,
+        targetGridPosition: { x: 3, z: 3 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+    ],
+    initialFurniture: [
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 0 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 1 },
+        angle: 90,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 2 },
+        angle: 270,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 3 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 4 },
+        angle: 180,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 1, z: 0 },
+        angle: 90,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 1, z: 1 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 1, z: 2 },
+        angle: 270,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 1, z: 3 },
+        angle: 90,
+      },
+      {
+        type: FURNITURE_TYPE.table1,
+        gridPosition: { x: 4, z: 5 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table2,
+        gridPosition: { x: 4, z: 4 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table1,
+        gridPosition: { x: 4, z: 3 },
+        angle: 0,
+      },
+    ],
+    robotCleaner: {
+      enabled: false,
+      position: { x: 0, z: 0 },
+      angle: 0,
+    },
+  },
+
+  // level 7
+  {
+    roomType: ROOM_TYPE.SMALL,
+    furniture: [
+      {
+        type: FURNITURE_TYPE.table_big,
+        targetGridPosition: { x: 1, z: 3 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.refrigerator,
+        targetGridPosition: { x: 5, z: 5 },
+        angle: 180,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        targetGridPosition: { x: 1, z: 5 },
+        angle: 90,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+    ],
+    initialFurniture: [
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 3 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 4 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 2, z: 3 },
+        angle: 180,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 2, z: 4 },
+        angle: 180,
+      },
+      {
+        type: FURNITURE_TYPE.nightstand,
+        gridPosition: { x: 5, z: 1 },
+        angle: 180,
+      },
+    ],
+    robotCleaner: {
+      enabled: false,
+      position: { x: 0, z: 0 },
+      angle: 0,
+    },
+  },
+
+  // level 8
+  {
+    roomType: ROOM_TYPE.SMALL,
+    furniture: [
+      {
+        type: FURNITURE_TYPE.table_big,
+        targetGridPosition: { x: 1, z: 3 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.washing_machine,
+        targetGridPosition: { x: 4, z: 3 },
+        angle: 180,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+      {
+        type: FURNITURE_TYPE.nightstand,
+        targetGridPosition: { x: 3, z: 1 },
+        angle: 90,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+    ],
+    initialFurniture: [
+      {
+        type: FURNITURE_TYPE.table1,
+        gridPosition: { x: 1, z: 1 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table2,
+        gridPosition: { x: 0, z: 4 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table1,
+        gridPosition: { x: 5, z: 1 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table2,
+        gridPosition: { x: 4, z: 5 },
+        angle: 0,
+      },
+    ],
+    robotCleaner: {
+      enabled: true,
+      position: { x: 0, z: 0 },
+      angle: 0,
+    },
+  },
+
+  // level 9
+  {
+    roomType: ROOM_TYPE.SMALL,
+    furniture: [
+      {
+        type: FURNITURE_TYPE.chair,
+        targetGridPosition: { x: 0, z: 3 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.refrigerator,
+        targetGridPosition: { x: 4, z: 1 },
+        angle: 90,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+      {
+        type: FURNITURE_TYPE.refrigerator,
+        targetGridPosition: { x: 3, z: 4 },
+        angle: 270,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        targetGridPosition: { x: 0, z: 5 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+    ],
+    initialFurniture: [
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 2 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        gridPosition: { x: 0, z: 4 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table_big,
+        gridPosition: { x: 1, z: 2 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.table_big,
+        gridPosition: { x: 1, z: 4 },
+        angle: 0,
+      },
+      {
+        type: FURNITURE_TYPE.refrigerator,
+        gridPosition: { x: 5, z: 5 },
+        angle: 180,
+      },
+    ],
+    robotCleaner: {
+      enabled: true,
+      position: { x: 0, z: 0 },
+      angle: 0,
+    },
+  },
+
+  // level 10
+  {
+    roomType: ROOM_TYPE.SMALL,
+    furniture: [
+      {
+        type: FURNITURE_TYPE.table_big,
+        targetGridPosition: { x: 1, z: 4 },
+        angle: 90,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.nightstand,
+        targetGridPosition: { x: 3, z: 2 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.left,
+      },
+      {
+        type: FURNITURE_TYPE.refrigerator,
+        targetGridPosition: { x: 0, z: 4 },
+        angle: 0,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.chair,
+        targetGridPosition: { x: 2, z: 5 },
+        angle: 90,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+      {
+        type: FURNITURE_TYPE.nightstand,
+        targetGridPosition: { x: 0, z: 5 },
+        angle: 90,
+        appearDirection: APPEAR_DIRECTION.right,
+      },
+    ],
+    initialFurniture: [
+      {
+        type: FURNITURE_TYPE.washing_machine,
+        gridPosition: { x: 5, z: 5 },
+        angle: 180,
+      },
+      {
+        type: FURNITURE_TYPE.gas_stove,
+        gridPosition: { x: 5, z: 1 },
+        angle: 180,
+      },
+    ],
+    robotCleaner: {
+      enabled: true,
+      position: { x: 0, z: 0 },
+      angle: 0,
     },
   },
 ];
